7eaac4a9e59ebec5352c7b16627fc23473e2424b
G-Dodge <40276048+G-Dodge@users.noreply.github.com>
2018-06-15 14:47:58 +0000

    Add files via upload

---
+++ dft.cpp
@@ -0,0 +1,42 @@
+﻿#include "stdafx.h"
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+using namespace cv;
+
+int main()
+{
+	Mat srcImage = imread("messi5.jpg", 0);
+	imshow("Original Image", srcImage);
+	int m = getOptimalDFTSize(srcImage.rows);
+	int n = getOptimalDFTSize(srcImage.cols);
+	Mat padded;
+	copyMakeBorder(srcImage, padded, 0, m - srcImage.rows, 0, n - srcImage.cols, BORDER_CONSTANT, Scalar::all(0));
+	Mat planes[] = { Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F) };
+	Mat complexI;
+	merge(planes, 2, complexI);
+	dft(complexI, complexI);
+	split(complexI, planes);
+	//planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+	magnitude(planes[0], planes[1], planes[0]);
+	Mat magnitudeImage = planes[0];
+	magnitudeImage += Scalar::all(1);
+	log(magnitudeImage, magnitudeImage);
+	magnitudeImage = magnitudeImage(Rect(0, 0, magnitudeImage.cols & -2, magnitudeImage.rows & -2));
+	int cx = magnitudeImage.cols / 2;
+	int cy = magnitudeImage.rows / 2;
+	Mat q0(magnitudeImage, Rect(0, 0, cx, cy));
+	Mat q1(magnitudeImage, Rect(cx, 0, cx, cy));
+	Mat q2(magnitudeImage, Rect(0, cy, cx, cy));
+	Mat q3(magnitudeImage, Rect(cx, cy, cx, cy));
+	Mat tmp;
+	q0.copyTo(tmp);
+	q2.copyTo(q0);
+	tmp.copyTo(q2);
+	normalize(magnitudeImage, magnitudeImage, 0, 1, NORM_MINMAX);
+	imshow("DFT IMAGEֵ", magnitudeImage);
+	waitKey(0);
+	return 0;
+}
+
